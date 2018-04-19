089fa8c90c98595293fe31f55b0a2faf56bec3ae
Aaron Bart-Addison <abartaddison12@gmail.com>
2018-04-19 19:26:43 -0400

    add nodemon

---
+++ server.js
@@ -0,0 +1,7 @@
+//import our dependencies
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
