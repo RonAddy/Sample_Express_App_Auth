60a16e60b55d601a33feb84b3427d0a21c7a9d78
Aaron Bart-Addison <abartaddison12@gmail.com>
2018-04-19 20:01:28 -0400

    add schema for database

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+\c auth_test
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    uname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password_digest VARCHAR(255)
+);
+
---
+++ models/User.js
@@ -0,0 +1,13 @@
+const db = require('../config/connection');
+
+const User = {};
+
+User.findById(id){
+    db.one(`
+        SELECT * 
+        FROM 
+    
+    
+    
+    `)
+}
