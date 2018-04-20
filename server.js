767103c15a581e8509a6ca25e16dc89c6cf6ffd5
Aaron Bart-Addison <abartaddison12@gmail.com>
2018-04-20 09:51:47 -0400

    adjust app to deliver lesson

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //allow us to access .env variables. Will be use to retrieve secret key for sessions
-require('dotenv').config()
+// require('dotenv').config()
 
 
 const express = require('express');
@@ -7,11 +7,11 @@ const logger = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-const session = require('express-session')
+// const session = require('express-session')
 //importing the authRouter into the app
-const authRouter = require('./services/auth/AuthRouter')
+// const authRouter = require('./services/auth/AuthRouter')
 
-const userRouter = require('./routes/user-router')
+// const userRouter = require('./routes/user-router')
 
 
 const PORT = process.env.PORT || 3000;
@@ -22,14 +22,14 @@ const app = express();
 app.use(logger('dev'));
 
 // set the secret using the SERVER_SECRET key stored in the .env file
-app.set('server_secret', process.env.SERVER_SECRET);
+// app.set('server_secret', process.env.SERVER_SECRET);
 
 //allow app to create session for users using SERVER_SECRET key. Other options are boilerplate.
-app.use(session({
-  secret:            app.get('server_secret'),
-  resave:            false,
-  saveUninitialized: false,
-}));
+// app.use(session({
+//   secret:            app.get('server_secret'),
+//   resave:            false,
+//   saveUninitialized: false,
+// }));
 
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -45,33 +45,33 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(methodOverride('_method'));
 
-app.use('/auth', authRouter);
-app.use('/users', userRouter)
+// app.use('/auth', authRouter);
+// app.use('/users', userRouter);
 
 app.get('/', (req, res) => {
     res.render('index');
 })
 
 //allow app to send a json object for routes our app does not recognize
-app.use("*", (err,req, res, next) => {
-    res.status(400).json({
-      error: err,
-      message: err.message
-    })
-})
+// app.use("*", (err,req, res, next) => {
+//     res.status(400).json({
+//       error: err,
+//       message: err.message
+//     })
+// })
 
 // render json object for server errors
-app.use((err, req, res,next) => {
-    console.log(err)
-    res.status(500).json({
-      error: err,
-      message: err.message
-    })
-  })
+// app.use((err, req, res,next) => {
+//     console.log(err)
+//     res.status(500).json({
+//       error: err,
+//       message: err.message
+//     })
+//   })
 
 app.listen(PORT,()=> {
-  console.log(`Server up and listening on port ${PORT}`);
+  console.log(`  🚕   💨     🏃  🚧  ${PORT} 🚧 `);
 })
-//render a json object for any route not defined by app
+
 
 
--- services/auth/AuthRouter.js
+++ services/auth/AuthRouter.js
@@ -1,18 +1,18 @@
-const authRouter = require('express').Router();
-const AuthServices = require('../auth/AuthServices');
-const usersViewController  = require("../../controllers/usersView-controller");
+// const authRouter = require('express').Router();
+// const AuthServices = require('../auth/AuthServices');
+// const usersViewController  = require("../../controllers/usersView-controller");
 
 
-authRouter.get('/login', usersViewController.showLoginForm)
-authRouter.post('/login', AuthServices.login, usersViewController.handleUserProfile)
+// authRouter.get('/login', usersViewController.showLoginForm)
+// authRouter.post('/login', AuthServices.login, usersViewController.handleUserProfile)
 
-authRouter.get('/register', usersViewController.showRegisterForm)
-authRouter.post('/register',AuthServices.register, usersViewController.handleUserProfile)
+// authRouter.get('/register', usersViewController.showRegisterForm)
+// authRouter.post('/register',AuthServices.register, usersViewController.handleUserProfile)
 
-/* Error handler */
-authRouter.use((err, req, res, next) => {
-    console.error(err);
-    res.json({ error: err });
-  });
+// /* Error handler */
+// authRouter.use((err, req, res, next) => {
+//     console.error(err);
+//     res.json({ error: err });
+//   });
 
-  module.exports = authRouter
+//   module.exports = authRouter
